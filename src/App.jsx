60bc72ee6a508421dc1f078d5bcde0da5ba08afc
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-08-07 22:40:33 +0530

    added context

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Home from './pages/Home'
+import ChatState from './Context/Chat/ChatState'
 
 function App() {
 
   return (
     <>
+      <ChatState>
       <Home />
+      </ChatState>
     </>
   )
 }
