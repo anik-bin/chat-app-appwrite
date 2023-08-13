bf5ece759d54e3eaba38fe7e117ec31b41eaa4fd
Aniket Bindhani <aniketbindhani44@gmail.com>
2023-08-13 16:37:42 +0530

    Moved AuthProvider to higher order than ChatState to fix a bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PrivateRoutes from './Components/PrivateRoutes'
+import { AuthProvider } from './Context/Auth/AuthContext'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import ChatState from './Context/Chat/ChatState'
 
 function App() {
 
   return (
     <>
-      <ChatState>
-      <div className="container">
-      <Home />
-      </div>
-      </ChatState>
+      <BrowserRouter>
+        <AuthProvider> 
+          <ChatState>
+            <div className="container">
+              <Routes>
+                <Route element={<PrivateRoutes />}>
+                  <Route path='/' element={<Home />} />
+                </Route>
+                <Route path='Login/*' element={<Login />} />
+                <Route path='Register/*' element={<Register />} />
+              </Routes>
+            </div>
+          </ChatState>
+        </AuthProvider>
+      </BrowserRouter>
     </>
   )
 }
